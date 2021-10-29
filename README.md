6a3639994bf7262185a692549d51659c5c957d5b
ManikandanKumarkp <manikandankumarkp@gmail.com>
2021-10-29 08:29:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-jgmmd2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-jgmmd2)
---
+++ index.js
@@ -0,0 +1,21 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS My Script</h1>`;
+
+//let fullname='mani'
+//fullname='siva'
+//console.log('name:',fullname)
+
+let lastname='siva'
+console.log('variable value:',lastname)
+
+
+
+
+
+
+
+
